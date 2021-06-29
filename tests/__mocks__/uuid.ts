eec19856867860e7616e86f0ef20ef7bdd2e8317
Rémi Rahir <rar@odoo.com>
2021-06-29 12:51:13 +0200

    isolate probelatic uuids ?

--- tests/__mocks__/uuid.ts
+++ tests/__mocks__/uuid.ts
@@ -3,11 +3,18 @@
  * */
 
 export class UuidGenerator {
+  static genId = 0;
+  private instance: number;
+  constructor() {
+    this.instance = UuidGenerator.genId++;
+    console.log(this.instance, "call generator constructor");
+  }
   private nextId = 1;
 
   setIsFastStrategy(isFast: boolean) {}
 
-  uuidv4(): string {
+  uuidv4(arg): string {
+    console.log(this.instance, arg);
     return String(this.nextId++);
   }
 
