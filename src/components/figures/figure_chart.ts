d831be58cc7d91d072622f44c8793ee199750761
Adrien Minne <adrm@odoo.com>
2022-03-23 12:16:40 +0100

    [REF] charts: separate chart and chart figure

--- src/components/figures/figure_chart.ts
+++ src/components/figures/figure_chart.ts
@@ -1,5 +1,4 @@
-import { Component, onMounted, onPatched, useRef, useState, xml } from "@odoo/owl";
-import Chart, { ChartConfiguration } from "chart.js";
+import { Component, useRef, useState, xml } from "@odoo/owl";
 import { BACKGROUND_CHART_COLOR, MENU_WIDTH } from "../../constants";
 import { MenuItemRegistry } from "../../registries/index";
 import { _lt } from "../../translation";
@@ -8,11 +7,12 @@ import { css } from "../helpers/css";
 import { useAbsolutePosition } from "../helpers/position_hook";
 import { LIST } from "../icons";
 import { Menu, MenuState } from "../menu";
+import { BasicChart } from "./basic_chart";
 
 const TEMPLATE = xml/* xml */ `
 <div class="o-chart-container" t-ref="chartContainer">
   <div class="o-chart-menu" t-on-click="showMenu">${LIST}</div>
-  <canvas t-att-style="canvasStyle" t-ref="graphContainer"/>
+    <BasicChart figure="props.figure"/>
   <Menu t-if="menuState.isOpen"
     position="menuState.position"
     menuItems="menuState.menuItems"
@@ -57,12 +57,10 @@ interface State {
 
 export class ChartFigure extends Component<Props, SpreadsheetChildEnv> {
   static template = TEMPLATE;
-  static components = { Menu };
+  static components = { Menu, BasicChart };
   private menuState: MenuState = useState({ isOpen: false, position: null, menuItems: [] });
 
-  canvas = useRef("graphContainer");
   private chartContainerRef = useRef("chartContainer");
-  private chart?: Chart;
   private state: State = { background: BACKGROUND_CHART_COLOR };
   private position = useAbsolutePosition(this.chartContainerRef);
 
@@ -70,56 +68,7 @@ export class ChartFigure extends Component<Props, SpreadsheetChildEnv> {
     return `background-color: ${this.state.background}`;
   }
 
-  setup() {
-    onMounted(() => {
-      const figure = this.props.figure;
-      const chartData = this.env.model.getters.getChartRuntime(figure.id);
-      if (chartData) {
-        this.createChart(chartData);
-      }
-    });
-
-    onPatched(() => {
-      const figure = this.props.figure;
-      const chartData = this.env.model.getters.getChartRuntime(figure.id);
-      if (chartData) {
-        if (chartData.type !== this.chart!.config.type) {
-          // Updating a chart type requires to update its options accordingly, if feasible at all.
-          // Since we trust Chart.js to generate most of its options, it is safer to just start from scratch.
-          // See https://www.chartjs.org/docs/latest/developers/updates.html
-          // and https://stackoverflow.com/questions/36949343/chart-js-dynamic-changing-of-chart-type-line-to-bar-as-example
-          this.chart && this.chart.destroy();
-          this.createChart(chartData);
-        } else if (chartData.data && chartData.data.datasets) {
-          this.chart!.data = chartData.data;
-          if (chartData.options?.title) {
-            this.chart!.config.options!.title = chartData.options.title;
-          }
-        } else {
-          this.chart!.data.datasets = undefined;
-        }
-        this.chart!.config.options!.legend = chartData.options?.legend;
-        this.chart!.config.options!.scales = chartData.options?.scales;
-        this.chart!.update({ duration: 0 });
-      } else {
-        this.chart && this.chart.destroy();
-      }
-      const def = this.env.model.getters.getChartDefinition(figure.id);
-      if (def) {
-        this.state.background = def.background;
-      }
-    });
-  }
-
-  private createChart(chartData: ChartConfiguration) {
-    const canvas = this.canvas.el as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d")!;
-    this.chart = new window.Chart(ctx, chartData);
-    const def = this.env.model.getters.getChartDefinition(this.props.figure.id);
-    if (def) {
-      this.state.background = def.background;
-    }
-  }
+  setup() {}
 
   showMenu(ev: MouseEvent) {
     const registry = new MenuItemRegistry();
