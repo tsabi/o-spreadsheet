6c3d1c31c18cb3c8958f291ae65ab928a6984381
Rémi Rahir <rar@odoo.com>
2022-10-21 09:12:36 +0200

    wip

--- src/components/figures/figure_container/figure_container.ts
+++ src/components/figures/figure_container/figure_container.ts
@@ -21,7 +21,7 @@ export class FiguresContainer extends Component<Props, SpreadsheetChildEnv> {
     onMounted(() => {
       // horrible, but necessary
       // the following line ensures that we render the figures with the correct
-      // viewport.  The reason is that whenever we initialize the grid
+      // viewport. The reason is that whenever we initialize the grid
       // component, we do not know yet the actual size of the viewport, so the
       // first owl rendering is done with an empty viewport.  Only then we can
       // compute which figures should be displayed, so we have to force a
